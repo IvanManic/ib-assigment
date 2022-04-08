02a13b46dfe24a0ced05444df7837f302f0bfb64
Ivan Manić <imanic@cs.hr>
2022-04-08 16:33:39 +0200

    ENV var added for docker/non docker environment

--- backend/routes/shorten.js
+++ backend/routes/shorten.js
@@ -4,8 +4,15 @@ var validator = require('../public/controllers/isValidURL')
 var miniUrl = require('../public/controllers/shortUrlGen')
 var mongoose = require('mongoose')
 
+var mongo_conn_str = ''
+console.log("Env: " + process.env.SHORTNER_ENV)
+if (process.env.SHORTNER_ENV === 'DOCKER') {
+    mongo_conn_str = 'mongodb://admin:password@mongo-db:27017/shortner'
+} else {
+    mongo_conn_str = 'mongodb://localhost:27017/shortner'
+}
 
-mongoose.connect('mongodb://admin:password@mongo-db:27017/shortner', {
+mongoose.connect(mongo_conn_str, {
     useNewUrlParser: true, useUnifiedTopology: true
 })
 router.post('/', (req, res) => {
